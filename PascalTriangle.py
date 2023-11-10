1cc6d4d93a943265415361d9b18f104ce09cf854
kurraNikhil <kurranikhil900@gmail.com>
2023-11-10 10:23:23 +0530

    PascalTriangle

---
+++ PascalTriangle.py
@@ -0,0 +1,19 @@
+# generates the nth row of Pascal's Triangle
+def pascalRow(n):
+    if n == 0:
+        return [1]
+    else:
+        N = pascalRow(n-1)
+        return [1] + [N[i] + N[i+1] for i in range(n-1)] + [1]
+
+def pascalTriangle(n):
+    triangle = []
+    for i in range(n):
+        triangle.append(pascalRow(i))
+    return triangle
+
+if __name__ == '__main__':
+    for i in pascalTriangle(7):
+        print(i)
+
+
